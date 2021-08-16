07faa6542c5bee52d183fe1844b58574fd53de65
OldBeliver <38506338004@yandex.ua>
2021-08-16 09:39:10 +0300

    class Detail

--- ServiceStation_02/Program.cs
+++ ServiceStation_02/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ServiceStation_02
+﻿using System;
+
+namespace ServiceStation_02
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
+
+    class Detail
+    {
+        public string Name { get; private set; } 
+        public int Condition { get; private set; }
+        public int Price { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} состояние {Condition}, {Price} рублей");
+        }
+    }
+    
+    
 }
