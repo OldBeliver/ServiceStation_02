375272c1f7320a16bae5e0ff6ffbb651b6c9f069
OldBeliver@outlook.com <OldBeliver@outlook.com>
2021-08-30 07:06:29 +0300

    add class Slot

--- ServiceStation_02/Program.cs
+++ ServiceStation_02/Program.cs
@@ -9,11 +9,11 @@ namespace ServiceStation_02
         {
             Creator creator = new Creator();
             List<Car> cars = new List<Car>();
-            List<Store> store = new List<Store>();
-            
+            Store store = new Store();
+
 
             for (int i = 0; i < 5; i++)
-            {   
+            {
                 cars.Add(creator.CreateNewCar());
             }
 
@@ -24,32 +24,33 @@ namespace ServiceStation_02
                 Console.WriteLine($"-----------------------");
             }
 
-            store.Add(creator.CreateNewStore());
+            store = creator.CreateNewStore();
 
             Console.WriteLine($"-------------------");
             Console.WriteLine($"склад");
-            Console.WriteLine($"№№  название\t\tсостояние\tстоимость");
-            for (int i = 0; i < store.Count; i++)
+            Console.WriteLine($"№№  название\t\tколичество\tстоимость");
+            Console.WriteLine($"--------------------------------------------------");
+            
+            store.ShowInfo();
+
+            Console.WriteLine($"нажите любую для наполнения склада");
+            Console.ReadKey();
+            
+            int count = store.GetSlotQuantity();
+
+            for (int i = 0; i < count; i++)
             {
-                store[i].ShowInfo();
-                Console.WriteLine($"-----------------------");
+                store.AddQuantity(count);
+
+                Console.Clear();
+                store.ShowInfo();
             }
         }
     }
 
     class ServiceCenter
     {
-        List<int> _detailQuantity;
-
-        public ServiceCenter()
-        {   
-            LoadZeroDetailQuantity();
-        }
-
-        private void LoadZeroDetailQuantity()
-        {
-            _detailQuantity = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0 };
-        }
+        
     }
 
     class Creator
@@ -57,6 +58,7 @@ namespace ServiceStation_02
         private List<Detail> _details;
         private Car _car;
         private Store _store;
+        private List<Slot> _slots;
         private static Random _random;
         private int _durable;
 
@@ -68,6 +70,7 @@ namespace ServiceStation_02
         public Creator()
         {
             _details = new List<Detail>();
+            _slots = new List<Slot>();
             _durable = 50;
 
             LoadDetails();
@@ -94,10 +97,10 @@ namespace ServiceStation_02
 
             for (int i = 0; i < _details.Count; i++)
             {
-                string name = _details[i].Name;
-                int price = _details[i].Price;
+                Detail detail = _details[i];
+                int quantity = 0;
 
-                _store.AddDetail(name, price);
+                _store.AddSlot(detail, quantity);
             }
 
             return _store;
@@ -106,7 +109,7 @@ namespace ServiceStation_02
         private void LoadDetails()
         {
             int condition = 100;
-            
+
             _details.Add(new Detail("амортизатор", condition, 685));
             _details.Add(new Detail("плановое ТО", condition, 5000));
             _details.Add(new Detail("ремень ГРМ ", condition, 255));
@@ -121,7 +124,8 @@ namespace ServiceStation_02
     class Car
     {
         private List<Detail> _details;
-        
+        private int usedDetailPrice = 0;
+
         public Car()
         {
             _details = new List<Detail>();
@@ -141,38 +145,79 @@ namespace ServiceStation_02
 
         public void AddDetail(string name, int condition)
         {
-            
-            _details.Add(new Detail(name, condition, 0));
+
+            _details.Add(new Detail(name, condition, usedDetailPrice));
         }
     }
 
     class Store
     {
-        private List<Detail> _detail;
+        private List<Slot> _slots;
+
 
         public Store()
         {
-            _detail = new List<Detail>();
+            _slots = new List<Slot>();
         }
 
         public void ShowInfo()
         {
-            for (int i = 0; i < _detail.Count; i++)
+            for (int i = 0; i < _slots.Count; i++)
             {
-                Console.Write($"{i+1:d2}. ");
-                _detail[i].ShowInfo();
+                Console.Write($"{i + 1:d2}. ");
+                _slots[i].ShowInfo();
             }
         }
 
-        public void AddDetail(string name, int price)
+        public void AddSlot(Detail detail, int quantity)
         {
-            _detail.Add(new Detail(name, 100, price));
+            _slots.Add(new Slot(detail, quantity));
+        }
+
+        public void AddQuantity(int index)
+        {
+            Console.Write($"количество: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            _slots[index].AddQuantity(number);
+        }
+
+        public int GetSlotQuantity()
+        {
+            return _slots.Count;
+        }
+    }
+
+    class Slot
+    {
+        private Detail _detail;
+        private int _quantity;
+
+        public Slot(Detail detail, int quantity)
+        {
+            _detail = detail;
+            _quantity = quantity;
+        }
+
+        public void ShowInfo()
+        {
+            //Console.WriteLine($"{_detail.Name} \t\t{_quantity} \t{_detail.Price}");
+            Console.WriteLine("{0,-1} \t{1,5:d2} \t{2,17:n2}", _detail.Name, _quantity, _detail.Price);
+        }
+
+        public void AddQuantity(int number)
+        {
+            _quantity += number;
+
+            if (_quantity < 0)
+            {
+                _quantity = 0;
+            }
         }
     }
 
     class Detail
     {
-        public string Name { get; private set; } 
+        public string Name { get; private set; }
         public int Condition { get; private set; }
         public int Price { get; private set; }
 
@@ -185,8 +230,8 @@ namespace ServiceStation_02
 
         public void ShowInfo()
         {
-            Console.WriteLine($"{Name} \t{Condition} \t\t{Price}");
-            //Console.WriteLine($"{Name} \t{Condition}");
+            //Console.WriteLine($"{Name} \t{Condition:d2} \t\t{Price}");
+            Console.WriteLine($"{Name} \t{Condition:d2}");
         }
 
         public void CreateNewCondition(int value)
